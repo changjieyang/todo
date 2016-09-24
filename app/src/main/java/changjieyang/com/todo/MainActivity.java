a78445cbf3ba33e7dc29ec499b7bdc741809354a
ChangYang <cyang@changjies-MacBook-Pro.local>
2016-09-24 11:15:18 -0700

    Initial commit

---
+++ app/src/main/java/changjieyang/com/todo/MainActivity.java
@@ -0,0 +1,13 @@
+package changjieyang.com.todo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
